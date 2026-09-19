21eb70055f8a9daecb817fecd75eb81ee0814692
challanger2000 <challanger2000@gmx.de>
2026-09-20 00:29:58 +0200

    Build initial Ugly Reverb sound-probe foundation

---
+++ source/controller.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "public.sdk/source/vst/vsteditcontroller.h"
+
+namespace UglyReverb {
+
+class Controller final : public Steinberg::Vst::EditControllerEx1
+{
+public:
+    static Steinberg::FUnknown* createInstance(void*) { return (Steinberg::Vst::IEditController*)new Controller(); }
+
+    Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) override;
+    Steinberg::tresult PLUGIN_API setComponentState(Steinberg::IBStream* state) override;
+};
+
+} // namespace UglyReverb
