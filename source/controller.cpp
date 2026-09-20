54f05ec52df066e80fa1f36fdeaf885c9ea95bbe
challanger2000 <challanger2000@gmx.de>
2026-09-20 17:28:59 +0200

    Label Digital Color modes as 12 BIT and 8 BIT

--- source/controller.cpp
+++ source/controller.cpp
@@ -113,7 +113,7 @@ VSTGUI::CView* Controller::createCustomView(VSTGUI::UTF8StringPtr name,
     if(auto*v=knob("Clang",kClang))return v; if(auto*v=knob("Rattle",kRattle))return v;
     if(auto*v=knob("Width",kWidth))return v;
     if(std::strcmp(name,"Digital")==0)
-        return new UglySelector(r,e,kDigital,std::vector<std::string>{"CLEAN","12","8"});
+        return new UglySelector(r,e,kDigital,std::vector<std::string>{"CLEAN","12 BIT","8 BIT"});
     if(auto*v=knob("Mix",kMix))return v; if(auto*v=knob("Output",kOutput))return v;
     if(std::strcmp(name,"Bypass")==0) return new UglyToggle(r,e,kBypass);
     return nullptr;
