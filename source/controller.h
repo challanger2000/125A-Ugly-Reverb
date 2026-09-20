312097316a59756214cbca2a1dbeccabd68bd7e7
challanger2000 <challanger2000@gmx.de>
2026-09-20 17:32:51 +0200

    Persist GUI zoom in controller state

--- source/controller.h
+++ source/controller.h
@@ -13,11 +13,16 @@ public:
     static Steinberg::FUnknown* createInstance(void*) { return (Steinberg::Vst::IEditController*)new Controller(); }
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) override;
     Steinberg::tresult PLUGIN_API setComponentState(Steinberg::IBStream* state) override;
+    Steinberg::tresult PLUGIN_API setState(Steinberg::IBStream* state) override;
+    Steinberg::tresult PLUGIN_API getState(Steinberg::IBStream* state) override;
     Steinberg::IPlugView* PLUGIN_API createView(const char* name) override;
     VSTGUI::CView* createCustomView(VSTGUI::UTF8StringPtr name,
         const VSTGUI::UIAttributes& attributes,
         const VSTGUI::IUIDescription* description,
         VSTGUI::VST3Editor* editor) override;
+
+private:
+    int guiZoomIndex_ {0};
 };
 
 } // namespace UglyReverb
