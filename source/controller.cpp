cb3fbd8b54cb22680bd3f47d6a4911575019e8d5
challanger2000 <challanger2000@gmx.de>
2026-09-20 19:36:14 +0200

    Register wear and glass texture layers

--- source/controller.cpp
+++ source/controller.cpp
@@ -145,7 +145,8 @@ VSTGUI::CView* Controller::createCustomView(VSTGUI::UTF8StringPtr name,
     VSTGUI::CPoint o{0,0},s{60,60}; a.getPointAttribute("origin",o); a.getPointAttribute("size",s);
     VSTGUI::CRect r(o.x,o.y,o.x+s.x,o.y+s.y);
     if(std::strcmp(name,"Faceplate")==0) return new UglyFaceplate(r);
-    if(std::strcmp(name,"GlassOverlay")==0) return new UglyGlassOverlay(r);
+    if(std::strcmp(name,"WearOverlay")==0) return new UglyTextureOverlay(r,"ugly_wear_overlay.png",1.f);
+    if(std::strcmp(name,"GlassOverlay")==0) return new UglyTextureOverlay(r,"ugly_glass_overlay.png",1.f);
     if(std::strcmp(name,"BrandLogo")==0) return new UglyLogo(r);
     if(std::strcmp(name,"GuiZoom")==0) return new UglyZoomControl(r,e,&guiZoomIndex_);
     auto knob=[&](const char* n,ParamID id)->VSTGUI::CView*{
