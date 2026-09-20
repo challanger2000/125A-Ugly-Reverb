58c23cf789679ae635465c562dde2e5422e76409
challanger2000 <challanger2000@gmx.de>
2026-09-20 05:34:44 +0200

    Enable VSTGUI custom hardware views

--- source/controller.h
+++ source/controller.h
@@ -1,16 +1,23 @@
 #pragma once
 #include "public.sdk/source/vst/vsteditcontroller.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+#include "vstgui/uidescription/uidescription.h"
+#include "vstgui/uidescription/uiattributes.h"
 
 namespace UglyReverb {
 
-class Controller final : public Steinberg::Vst::EditControllerEx1
+class Controller final : public Steinberg::Vst::EditControllerEx1,
+                         public VSTGUI::VST3EditorDelegate
 {
 public:
     static Steinberg::FUnknown* createInstance(void*) { return (Steinberg::Vst::IEditController*)new Controller(); }
-
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) override;
     Steinberg::tresult PLUGIN_API setComponentState(Steinberg::IBStream* state) override;
     Steinberg::IPlugView* PLUGIN_API createView(const char* name) override;
+    VSTGUI::CView* createCustomView(VSTGUI::UTF8StringPtr name,
+        const VSTGUI::UIAttributes& attributes,
+        const VSTGUI::IUIDescription* description,
+        VSTGUI::VST3Editor* editor) override;
 };
 
 } // namespace UglyReverb
